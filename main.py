6951020d8e83bd3de4360884dfa3af284f938ccc
Angel-alexander53 <aasantos@scl.edu.gt>
2024-09-06 11:41:33 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as robot
+
+
+#crear ventana
+mi_ventana =robot.Tk()
+mi_ventana.title("angel Santos ")
+mi_ventana.geometry("500x600")
+
+
+#crear un texto
+#crea para un texto arriba
+etiqueta = robot.Label(mi_ventana,text ="hola mi rey")
+etiqueta.pack()
+
+
+
+mi_ventana.mainloop()
